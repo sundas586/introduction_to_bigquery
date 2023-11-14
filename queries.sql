508e3a127eab32febbe286e97d189138e7988e23
Sundas A <33677647+sundas586@users.noreply.github.com>
2023-11-14 12:25:56 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,3 +6,7 @@ FROM bigquery-public-data.stackoverflow.__TABLES__;
 
 SELECT project_id, dataset_id, table_id as table_name, CAST(TIMESTAMP_MILLIS(creation_time) AS DATETIME) as creation_time,  CAST(TIMESTAMP_MILLIS(last_modified_time) AS DATETIME) as last_modified_time, row_count, size_bytes / POW(10,9) as GB, type
 FROM bigquery-public-data.stackoverflow.__TABLES__;
+
+SELECT *
+FROM bigquery-public-data.stackoverflow.INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'posts_questions'
